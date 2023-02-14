5ac18787108b816fe603195525aa56d087567e12
Matthias Mattanovich <57752712+mmattano@users.noreply.github.com>
2023-02-14 12:09:34 +0900

    setup of progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from datetime import datetime
+from datetime import date
 
 """
 # Welcome to Streamlit!
@@ -11,28 +13,20 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.title('Welcome to the /"Matthias loves and misses Holda very much/" app!')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.write('Progress of the external stay')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+def get_difference(date1, date2):
+    delta = date2 - date1
+    return delta.days
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+d1 = datetime(2023, 2, 12)
+d_current = datetime.now()
+d2 = datetime(2023, 5, 13)
+days_total = get_difference(d1, d2)
+days_past = get_difference(d1, d_current)
+ratio = days_past / days_total
+st.progress(ratio)
