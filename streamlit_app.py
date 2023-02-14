96739f6b49e87feb07a94be5b3979b827769e303
Matthias Mattanovich <57752712+mmattano@users.noreply.github.com>
2023-02-14 12:35:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,6 @@ import streamlit as st
 from datetime import datetime
 #from streamlit_extras import let_it_rain
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-"""
 
 st.title('Welcome to the "Matthias loves and misses Holda very much" app!')
 
